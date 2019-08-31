062700bb55ca576f29cfcd18600f558158550258
Some <itsmeguy@hotmail.com>
2019-08-31 16:19:05 -0400

    Organization method of stocked items

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,13 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		// *Find number of objects in store, list their names and prices
+		int itemsInStock = scan.nextInt();
+		String[] itemsList = new String[itemsInStock];
+		double[] itemsPrice = new double[itemsInStock];
+		for (int i = 0; i < itemsInStock; i++) {
+			itemsList[i] = scan.next();
+			itemsPrice[i] = scan.nextDouble();
+		}
 	}
 }
