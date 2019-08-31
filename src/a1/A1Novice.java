e1a46f776d8d922719a31a42a9c35c7dde850646
Some <itsmeguy@hotmail.com>
2019-08-31 15:53:54 -0400

    1st Attempt, double for loop

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,28 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		/* 1. Find out how many customers we need to scan through
+		 *  2. Use that number in a loop to go through the shopping list
+		 *  of each customer
+		 */
+		int customers = scan.nextInt();
+		for (int i = 0; i > customers; i++) {
+			// 3. Gather the first initial and last name of the customer
+			String firstName = scan.next();
+			char firstInitial = firstName.charAt(0);
+			String lastName = scan.next();
+			// 4. Find out how many items they purchased
+			int items = scan.nextInt();
+			// 5. Add up the cost of all items purchased
+			double cost = 0;
+			for (int j = 0; j < items; j++) {
+				int purchases = scan.nextInt();
+				double price = scan.nextDouble();
+				cost += (purchases * price);
+			}
+			// 6. Print out the "receipt" of the customer
+			System.out.print(firstInitial + ". " + lastName + ": " + cost);
+		}
+		scan.close();
 	}
 }
